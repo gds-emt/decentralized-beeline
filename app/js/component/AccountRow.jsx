f43d543d0bc5c7f03126f0dd9a499e734d5022c5
U-Zyn Chua <chua@uzyn.com>
2016-11-11 01:21:04 +0800

    Some stylings

--- app/js/component/AccountRow.jsx
+++ app/js/component/AccountRow.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { web3 } from '../../../contract/Beeline.sol';
+import { Beeline, web3 } from '../../../contract/Beeline.sol';
+import DisplayEth from '../helper/DisplayEth.jsx';
 
 const propTypes = {
   account: React.PropTypes.string.isRequired,
 };
 
-class AccountRow extends React.Component {
-  render() {
-    return (
+export default function AccountRow(props) {
+  return (
       <tr className="Web3">
-        <td>{this.props.account}</td>
-        <td>{web3.eth.getBalance(this.props.account).toNumber()}</td>
-        <td>(withdrawable)</td>
+        <th>{props.account}</th>
+        <td><DisplayEth wei={web3.eth.getBalance(props.account)} /></td>
+        <td><DisplayEth wei={Beeline.balances(props.account)} /></td>
         <td>(has tickets)</td>
         <td>
           <button>Add route</button>
@@ -19,19 +19,6 @@ class AccountRow extends React.Component {
         </td>
       </tr>
     );
-  }
 }
 
 AccountRow.propTypes = propTypes;
-
-export default AccountRow;
-/*
-export default function AccountRow() {
-//  console.log(this.props);
-  return (
-    <tr className="Web3">
-      {this.props.account}
-    </tr>
-  );
-}
-*/
--- app/js/component/Accounts.jsx
+++ app/js/component/Accounts.jsx
@@ -5,30 +5,33 @@ import AccountRow from './AccountRow';
 export default function Accounts() {
   return (
     <div className="Web3">
-      <h3>Accounts</h3>
-      <table>
-        <tr>
-          <th></th>
-          <th>Balance</th>
-          <th>Withdrawable</th>
-          <th>Has tickets</th>
-          <th></th>
-        </tr>
-
-          {web3.eth.accounts.map(
-            (account) => <AccountRow key={account} account={account} />
-          )}
-      </table>
-
       <h3>Blockchain</h3>
-      <dl>
-        <dt>Connected Ethereum node (Web3 provider)</dt>
+      <dl className="dl-horizontal">
+        <dt>Ethereum node</dt>
         <dd>{web3.currentProvider.host}</dd>
         <dt>Latest block</dt>
         <dd>{web3.eth.blockNumber}</dd>
-        <dt>Beeline contract address</dt>
+        <dt>Beeline contract</dt>
         <dd>{Beeline.address}</dd>
       </dl>
+
+      <h3>Accounts</h3>
+      <table className="table table-hover table-condensed table-striped">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Balance</th>
+            <th>Withdrawable</th>
+            <th>Has tickets</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {web3.eth.accounts.map(
+            (account) => <AccountRow key={account} account={account} />
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
